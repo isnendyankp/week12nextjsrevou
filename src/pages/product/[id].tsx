75eae31d913d443afc5779df8b8b0e6c23641767
isnendyankp <isnendyankp@gmail.com>
2024-01-18 16:39:36 +0700

    cr8 product by id

---
+++ src/pages/product/[id].tsx
@@ -0,0 +1,15 @@
+const ProductById = () => {
+
+    
+
+    return (
+        <div>
+            {' ini adalah halaman product by id '}
+        </div>
+    )
+}
+
+export default ProductById
+
+// explaination:
+// 1. check web browser with url: http://localhost:3000/product/1
