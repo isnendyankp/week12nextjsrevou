e5b8d44821620fba9b4860630cb52ecd4e07831e
isnendyankp <isnendyankp@gmail.com>
2024-01-18 19:58:55 +0700

    cr8 base AppProvider

--- src/providers/AppProvider.tsx
+++ src/providers/AppProvider.tsx
@@ -6,5 +6,11 @@ interface User {
     email: string;
 }
 
-// 
+// cr8 context with using function createContext
 export const AppContext = createContext(undefined);
+
+// cr8 AppProvider function
+
+const AppProvider = ({ children }) => {
+    
+};
