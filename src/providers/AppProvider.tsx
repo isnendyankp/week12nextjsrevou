ae6c4eef0eb7870f2ecfd061f7db7fb1c1d1b666
isnendyankp <isnendyankp@gmail.com>
2024-01-18 20:00:19 +0700

    cr8 Props interface

--- src/providers/AppProvider.tsx
+++ src/providers/AppProvider.tsx
@@ -6,11 +6,16 @@ interface User {
     email: string;
 }
 
+// interface Props
+interface Props {
+    children: React.ReactNode;
+}
+
 // cr8 context with using function createContext
 export const AppContext = createContext(undefined);
 
 // cr8 AppProvider function
 
 const AppProvider = ({ children }) => {
-    
+
 };
