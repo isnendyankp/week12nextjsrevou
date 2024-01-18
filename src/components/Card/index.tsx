dfc68340ceda09a36a4fc77efaed53873300cbb3
isnendyankp <isnendyankp@gmail.com>
2024-01-19 06:36:41 +0700

    fix card component

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,5 +1,5 @@
 import { ReactNode, useMemo } from 'react';
-import style from './Card.module.css'
+import style from './Card.module.css';
 
 interface Props {
     children: ReactNode;
@@ -8,30 +8,30 @@ interface Props {
     direction?: 'row' | 'column';
 }
 
-const Card = ({ children, justifyContent = 'start, display = 'flex', direction = 'row'} : Props) => {
+const Card = ({ children, justifyContent = 'start', display = 'flex', direction = 'row' }: Props) => {
 
-    // cr8 a style obj(justityContentStyled) to hold the styles for the card component
+    // cr8 a style obj(justifyContentStyled) to hold the styles for the card component
     const justifyContentStyled = useMemo(
-       () => {
-        if (justifyContent === 'start') {
-            return style.justifyLeft
-        }
-        if (justifyContent === 'center') {
-            return style.justifyCenter
-        }
+        () => {
+            if (justifyContent === 'start') {
+                return style.justifyLeft;
+            }
+            if (justifyContent === 'center') {
+                return style.justifyCenter;
+            }
 
-        return style.justifyRight
-       }, 
-       [justifyContent]
+            return style.justifyRight;
+        },
+        [justifyContent]
     );
 
     // cr8 displayStyled for the card component
     const displayStyled = useMemo(
         () => {
             if (display === 'flex') {
-                return style.displayFlex
+                return style.displayFlex;
             }
-            return style.displayBlock
+            return style.displayBlock;
         },
         [display]
     );
@@ -40,21 +40,21 @@ const Card = ({ children, justifyContent = 'start, display = 'flex', direction =
     const directionStyle = useMemo(
         () => {
             if (direction === 'row') {
-                return style.directionRow
+                return style.directionRow;
             }
             if (direction === 'column') {
-                return style.directionColumn
+                return style.directionColumn;
             }
         },
         [direction]
     );
-    
+
     // return the card component
     return (
-        <div className={`${style.cardContainer} ${justifyContentStyled} ${displaStyled} ${directionStyle}`}>
+        <div className={`${style.cardContainer} ${justifyContentStyled} ${displayStyled} ${directionStyle}`}>
             {children}
         </div>
-    )
-}
+    );
+};
 
 export default Card;
