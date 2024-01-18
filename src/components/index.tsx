7fdbafff852df052cc2bb815941a1fd80c770af2
isnendyankp <isnendyankp@gmail.com>
2024-01-19 06:10:01 +0700

    add conditional statement for justifyContent

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -9,9 +9,17 @@ interface Props {
 
 const Card = ({ children, justifyContent = 'start'} : Props) => {
 
+    // cr8 a style obj(justityContentStyled) to hold the styles for the card component
     const justifyContentStyled = useMemo(
        () => {
-        
+        if (justifyContent === 'start') {
+            return style.justifyLeft
+        }
+        if (justifyContent === 'center') {
+            return style.justifyCenter
+        }
+
+        return style.justifyRight
        }
     );  
 }
