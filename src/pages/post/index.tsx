e73327dcd063375cc0f0535b29e1929290f3b2ad
isnendyankp <isnendyankp@gmail.com>
2024-01-18 20:27:09 +0700

    Add state for post in Post component

--- src/pages/post/index.tsx
+++ src/pages/post/index.tsx
@@ -1,8 +1,12 @@
+// interface
 interface Post{
     title: string;
 }
 
 const Post = () => {
+
+    // cr8 state post
+    const [post, setPost] = useState<Post[]>([])
 
     return (
         <div>
