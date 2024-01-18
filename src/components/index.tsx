c35e928d4464b4003163a999fd8cf1ef1d20225e
isnendyankp <isnendyankp@gmail.com>
2024-01-19 06:02:59 +0700

    cr8 card components with interface Props

---
+++ src/components/index.tsx
@@ -0,0 +1,13 @@
+import exp from "constants";
+
+
+interface Props {
+    children: ReactNode;
+    justifyContent?: 'start' | 'center' | 'end';
+    display?: 'flex' | 'block';
+}
+
+const Card = () => {
+}
+
+export default Card;
