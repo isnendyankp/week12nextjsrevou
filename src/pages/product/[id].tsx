9e265165e33012f39c3da0eecb360bc438d56da0
isnendyankp <isnendyankp@gmail.com>
2024-01-18 16:54:15 +0700

    fix code with backtick

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -5,14 +5,12 @@ const ProductById = () => {
     const router = useRouter()
 
     return (
-        <div>
-            {' ini adalah halaman product by id: ${router.query.id} '}
-        </div>
-    )
+      <div>{`ini adalah halaman product by id: ${router.query.id}`}</div>
+    );
 }
 
 export default ProductById
 
 // explaination:
 // 1. check web browser with url: http://localhost:3000/product/1
-// 2. pass id as parameter in url with router.query.id : {' ini adalah halaman product by id: ${router.query.id} '}
+// 2. pass id as parameter in url with router.query.id : {` ini adalah halaman product by id: ${router.query.id} `}
