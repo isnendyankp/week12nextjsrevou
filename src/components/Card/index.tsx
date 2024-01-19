429fe1e7124a273ad0476828ec67f7aa3882c34c
isnendyankp <isnendyankp@gmail.com>
2024-01-19 09:57:07 +0700

    cr8 interface Props & cr8 base card Component

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,2 +1,14 @@
 import styled from "styled-components";
+import { CSS, CSSProp, CSSProperties } from "styled-components/dist/types";
 
+interface Props {
+    children: ReactNode;
+    display: CSSProperties['display'];
+    className?: string;
+}
+
+const Card = () => {
+    return (
+
+    )
+};
