69d03dbf265f060ddd0d86c0fdd7b7541fd157e9
isnendyankp <isnendyankp@gmail.com>
2024-01-19 10:33:52 +0700

    add wrap for interface, BaseCard & pass in into Card components

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -6,17 +6,19 @@ interface Props {
     display?: CSSProperties['display'];
     className?: string;
     direction?: CSSProperties['flexDirection'];
+    wrap?: CSSProperties['flexWrap'];
 }
 
 // Basecard
 const BaseCard = styled.div<Props>`
-    display: !${(props) => props.display};
-    flex-direction: !${(props) => props.direction};
+  display: ${(props) => props.display} !important;
+  flex-direction: ${(props) => props.direction} !important;
+    flex-wrap: ${(props) => props.wrap};
 `;
 
-const Card = ({ children, display = 'flex', direction = 'row',  className } : Props) => {
+const Card = ({ children, display = 'flex', direction = 'row',  className, wrap = 'wrap' } : Props) => {
     return (
-        <BaseCard display={display} className={className} direction={direction}>
+        <BaseCard display={display} className={className} direction={direction} wrap={wrap}>
             {children}
         </BaseCard>
     )
