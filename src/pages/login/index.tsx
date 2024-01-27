81e4c4f5cc726db1b02c574882dc4a6b8ecf7a26
isnendyankp <isnendyankp@gmail.com>
2024-01-27 14:22:09 +0700

    Add login form submission functionality

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -17,6 +17,40 @@ const Login = () => {
       password: '',
     },
 
+    // onSubmit function for login form
+    onSubmit: async (values) => {
+      try {
+        const response = await fetch(
+          'https://mock-api.arikmpt.com/api/user/login',
+          {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(values),
+          }
+        );
+
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        } else {
+          // if response is ok, you can use it here
+          const data = await response.json();
+          // set token to local storage
+          localStorage.setItem('token', data.data.token);
+          console.log(data);
+          // navigate to list page
+          router.push('/PokemonList');
+        }
+      } catch (error) {
+        // catch any network error or any error thrown in the fetch call
+        console.log(
+          'There has been a problem with your fetch operation: ' +
+            (error as Error).message
+        );
+      }
+    },
+
 
   return <div>{' ini adalah halaman Login '}</div>;
 };
