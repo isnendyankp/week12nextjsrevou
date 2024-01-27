17c6480fbefa39fc04466325bc51d00b69f78d24
isnendyankp <isnendyankp@gmail.com>
2024-01-27 14:13:15 +0700

    Add router hook to Login page

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,4 +1,9 @@
+import { useRouter } from 'next/router';
+
 const Login = () => {
+  // use Navigate router hook
+  const router = useRouter();
+
   return <div>{' ini adalah halaman Login '}</div>;
 };
 
