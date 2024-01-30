4f334309c103b62e4b267a889b41f959605680dc
isnendyankp <isnendyankp@gmail.com>
2024-01-30 14:44:17 +0700

    Add new components to index.ts for file distributor

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,6 @@
 // file distributor for components
 export { default as OldCard } from './OldCard';
 export { default as Card } from './Card';
+export { default as Input } from './Input';
+export { default as Text } from './Text';
+export { default as Button } from './Button';
