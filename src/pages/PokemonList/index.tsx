fcae87156542ed2c1fb44bd31ce393e787c6f59e
isnendyankp <isnendyankp@gmail.com>
2024-01-31 19:30:37 +0700

    Add useState hook for managing Pokemon list data

--- src/pages/PokemonList/index.tsx
+++ src/pages/PokemonList/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import { PokemonListData } from '../../interfaces/Category';
 
 const PokemonListContainer: React.FC = () => {
-
+  // useState hook
+  const [pokemon, setPokemon] = useState<PokemonListData[]>([]);
 }
 
 export default PokemonListContainer;
