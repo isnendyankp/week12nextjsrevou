a765368e35e37386bb34df7bf430dd1d5ba782f6
isnendyankp <isnendyankp@gmail.com>
2024-01-31 19:31:55 +0700

    Add useRouter hook to PokemonListContainer

--- src/pages/PokemonList/index.tsx
+++ src/pages/PokemonList/index.tsx
@@ -1,9 +1,13 @@
 import React, { useState } from 'react';
 import { PokemonListData } from '../../interfaces/Category';
 
+
 const PokemonListContainer: React.FC = () => {
   // useState hook
   const [pokemon, setPokemon] = useState<PokemonListData[]>([]);
+
+  // use Navigate router hook
+  const router = useRouter();
 }
 
 export default PokemonListContainer;
