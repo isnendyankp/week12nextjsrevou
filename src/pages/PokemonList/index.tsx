36d3c238eccb14109a480cb1431a25a82d865b8a
isnendyankp <isnendyankp@gmail.com>
2024-01-31 19:34:25 +0700

    import useEffect

--- src/pages/PokemonList/index.tsx
+++ src/pages/PokemonList/index.tsx
@@ -1,7 +1,6 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { PokemonListData } from '../../interfaces/Category';
 import { useRouter } from 'next/router';
-
 
 const PokemonListContainer: React.FC = () => {
   // useState hook
@@ -10,10 +9,8 @@ const PokemonListContainer: React.FC = () => {
   // use Navigate router hook
   const router = useRouter();
 
-    // useEffect hook
-    useEffect(() => {
-        
-    }, []);
-}
+  // useEffect hook
+  useEffect(() => {}, []);
+};
 
 export default PokemonListContainer;
