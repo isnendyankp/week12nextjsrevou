683bfd9197c7f18e5268b084e763048bf29c62cb
isnendyankp <isnendyankp@gmail.com>
2024-02-01 15:14:12 +0700

    Add Pokemon list container layout

--- src/pages/PokemonList/index.tsx
+++ src/pages/PokemonList/index.tsx
@@ -59,4 +59,12 @@ const PokemonListContainer: React.FC = () => {
     fetchPokemon();
   }, []);
 
+  // return pokemon list
+  return (
+     <div className="flex flex-col items-center justify-center bg-gray-100 min-h-screen px-6">
+     
+     </div>
+  )
+};
+
 export default PokemonListContainer;
