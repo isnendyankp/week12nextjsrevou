0229033a00b247fbfe393474fbfeab7836d00836
isnendyankp <isnendyankp@gmail.com>
2024-02-04 22:20:20 +0700

    Add useState hook for search functionality

--- src/pages/PokemonSearch/index.tsx
+++ src/pages/PokemonSearch/index.tsx
@@ -1,6 +1,10 @@
-import React from 'react'
+import React,{ useState } from 'react'
+import { PokemonSearchData } from '../../interfaces/Category';
 
 const PokemonSearchContainer: React.FC = () => {
+  // useState hook
+  const [search, setSearch] = useState('');
+  const [pokemon, setPokemon] = useState<PokemonSearchData | null>(null);
   return <div>index</div>;
 };
 
