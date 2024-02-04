e800e7b23148ed1877925e9c443a1787d24ac3db
isnendyankp <isnendyankp@gmail.com>
2024-02-04 22:20:44 +0700

    Add router hook for navigation

--- src/pages/PokemonSearch/index.tsx
+++ src/pages/PokemonSearch/index.tsx
@@ -5,6 +5,10 @@ const PokemonSearchContainer: React.FC = () => {
   // useState hook
   const [search, setSearch] = useState('');
   const [pokemon, setPokemon] = useState<PokemonSearchData | null>(null);
+
+  // use Navigate router hook
+  const router = useRouter();
+  
   return <div>index</div>;
 };
 
