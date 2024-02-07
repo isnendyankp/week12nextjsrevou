5937a724f6b142322e4f0797184d9bc43998f380
isnendyankp <isnendyankp@gmail.com>
2024-02-07 14:57:40 +0700

    Fix import and add missing newline at end of file

--- src/pages/PokemonSearch/index.tsx
+++ src/pages/PokemonSearch/index.tsx
@@ -1,4 +1,4 @@
-import React,{ use, useState } from 'react'
+import React, { useState, useEffect } from 'react';
 import { PokemonSearchData } from '../../interfaces/Category';
 import { useRouter } from 'next/router';
 import { useAuth } from '../../context/AuthContext';
